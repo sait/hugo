07abf07642b2caf720cd107673340896789a15de
HugoSait <hugosoportesait@gmail.com>
2022-11-25 16:41:34 -0700

    Insercion de quasar-project

---
+++ Vue3/quasar-project/src/pages/Help.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none"> Help</h5>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium a, nemo, provident officiis sed, alias deleniti nam itaque similique soluta facilis. Ex atque voluptas aspernatur, minus hic ipsam illum laboriosam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quaerat, cum natus nisi inventore amet voluptas cupiditate dolorem necessitatibus eos deserunt earum fuga quasi sint odit! Reprehenderit nulla aspernatur quo?</p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelpPage'
+})
+</script>
